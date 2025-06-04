d228c1b5ae2b2f210ec9f93cd507bfc3b068c2f4
vitalii-t12 <vitalii.toderian@gmail.com>
2025-06-04 21:23:03 +0300

    Use path for docker volume

--- mnl_factory/scripts/install-factory.sh
+++ mnl_factory/scripts/install-factory.sh
@@ -34,10 +34,10 @@ cd "$SETUP_SCRIPTS_DIR"
 
 # Download setup scripts
 print_message "Downloading setup scripts to $SETUP_SCRIPTS_DIR..." "$YELLOW"
-curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/main/mnl_factory/scripts/1_prerequisites.sh
-curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/main/mnl_factory/scripts/2_ansible_setup.sh
-curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/main/mnl_factory/scripts/3_configure.py
-curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/main/mnl_factory/scripts/4_run_setup.sh
+curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/gpu-fixes/mnl_factory/scripts/1_prerequisites.sh
+curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/gpu-fixes/mnl_factory/scripts/2_ansible_setup.sh
+curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/gpu-fixes/mnl_factory/scripts/3_configure.py
+curl -sO https://raw.githubusercontent.com/Ratio1/multi-node-launcher/refs/heads/gpu-fixes/mnl_factory/scripts/4_run_setup.sh
 
 # Make scripts executable
 chmod +x 1_prerequisites.sh 2_ansible_setup.sh 3_configure.py 4_run_setup.sh
